583cc85b3938d6c2ffb65111585d62783977a54c
Gustavo de Medeiros <gustavormedeiros2008@gmail.com>
2025-03-10 09:08:00 -0300

    Movimentação de 2 players

---
+++ Churrasco de Noob/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private string inputNameHorizontal;
+    [SerializeField] private string inputNameVertical;
+    [SerializeField] private Color color;
+
+    private Rigidbody rb;
+    private Renderer render;
+
+    private float inputHorizontal;
+    private float inputVertical;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        render = GetComponentInChildren<Renderer>();
+        render.material.color = color;
+    }
+
+    private void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw(inputNameHorizontal);
+        inputVertical = Input.GetAxisRaw(inputNameVertical);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(inputHorizontal, 0f, inputVertical).normalized;
+
+        if (moveDirection.magnitude > 0)
+        {
+            rb.linearVelocity = moveDirection * speed;
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
+        }
+    }
+
+
+}
