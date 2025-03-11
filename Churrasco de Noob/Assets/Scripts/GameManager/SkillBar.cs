6efdee642950ebc7256ea20d6c467f76cf116c80
Gustavo de Medeiros <gustavormedeiros2008@gmail.com>
2025-03-11 14:09:42 -0300

    Hud

---
+++ Churrasco de Noob/Assets/Scripts/GameManager/SkillBar.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillBar : MonoBehaviour
+{
+    public Slider slider;
+    public float decreaseRate = 1f; 
+    private float currentValue;
+
+    void Start()
+    {
+        if (slider == null)
+        {
+            return;
+        }
+
+        currentValue = slider.maxValue;
+        slider.value = currentValue;
+    }
+
+    void Update()
+    {
+        if (currentValue > 0)
+        {
+            currentValue -= decreaseRate * Time.deltaTime;
+            slider.value = currentValue;
+        }
+
+        if (currentValue <= 0)
+        {
+            OnSliderEmpty();
+        }
+    }
+
+    public void IncreaseSlider(float amount)
+    {
+        currentValue += amount;
+        if (currentValue > slider.maxValue)
+        {
+            currentValue = slider.maxValue;
+        }
+        slider.value = currentValue;
+    }
+
+    private void OnSliderEmpty()
+    {
+        Debug.Log("befana");
+    }
+}
